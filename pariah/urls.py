073404f098e626d8e9b32bd9101829c140b6c39a
Paul Hummer <paul@eventuallyanyway.com>
2012-02-26 23:18:06 -0700

    Don't make the archive list the default

--- pariah/urls.py
+++ pariah/urls.py
@@ -4,7 +4,7 @@ from pariah import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.ComicsView.as_view(), name='comic-list'),
+    url(r'archive$', views.ComicsView.as_view(), name='comic-list'),
     url(r'^(?P<year>\d{4})/(?P<month>[0-9]{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w ]+)/',
         views.ComicView.as_view(), name='comic-detail'),
 )
